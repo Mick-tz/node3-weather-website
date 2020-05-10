959b00ebca6ebe006fb5fc655b8ccf05421e516d
chino <mick.tz@hotmail.de>
2020-05-10 13:46:19 -0500

    Added precip probability

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,12 +1,3 @@
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
 
 const request = require("postman-request")
 
@@ -22,7 +13,8 @@ const forecast = (latitude, longitude, callback) => {
         } else {
             data = body.current
             callback(undefined, data.weather_descriptions[0] + '. It is currently ' + data.temperature + 
-                ' degrees out. But it feels like ' + data.feelslike + ' degrees!')
+                ' degrees out. But it feels like ' + data.feelslike + ' degrees!' +
+                 ' The precipipation probabilty is currently ' + data.precip)
     }
 }) 
 
